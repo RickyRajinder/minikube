837e999199d27e7db9f678c295723b6dcb1ded06
RickyRajinder <singh.sangh@gmail.com>
2019-09-30 19:38:55 -0700

    Define file_passes and file_extension

--- hack/boilerplate/boilerplate.go
+++ hack/boilerplate/boilerplate.go
@@ -17,13 +17,18 @@ limitations under the License.
 package main
 
 import (
+	"bufio"
 	"flag"
+	"io/ioutil"
+	"os"
 	"path"
 	"path/filepath"
+	"regexp"
 	"runtime"
+	"strings"
 )
 
-func createArgumentParser() *flag.FlagSet {
+func createCommandFlags() *flag.FlagSet {
 	flag.String("filenames", ".", "list of all files to check, all files if unspecified")
 	_, filename, _, _ := runtime.Caller(0)
 	rootdir, _ := filepath.Abs(path.Join(path.Dir(filename), "/../../"))
@@ -34,7 +39,42 @@ func createArgumentParser() *flag.FlagSet {
 	return flag.CommandLine
 }
 
+func getRefs(f *flag.FlagSet) map[string][]string {
+	refs := map[string][]string{}
+	glob, _ := filepath.Glob(path.Join(f.Lookup("--boilerplate-dir").Value.String(), "boilerplate.*.txt"))
+	for i := range glob {
+		extension := strings.Split(path.Base(glob[i]), ".")[1]
+		ref_file, _ := os.Open(glob[i])
+		scan := bufio.NewScanner(ref_file)
+		scan.Split(bufio.ScanLines)
+		var ref []string
+		for scan.Scan() {
+			ref = append(ref, scan.Text())
+		}
+		ref_file.Close()
+		refs[extension] = ref
+	}
+	return refs
+}
+
+func filePasses(filename string, refs map[string][]string, regexs map[string]*regexp.Regexp) bool {
+	f, err := os.Open(filename)
+	if err != nil {
+		return false
+	}
+	data, _ := ioutil.ReadAll(f)
+	f.Close()
+	basename := path.Base(filename)
+	extenstion := fileExtension(filename)
+}
+
+func fileExtension(filename string) string {
+	_, ext := path.Split(filename)
+	exts := strings.Split(path.Base(ext), ".")
+	return exts[len(exts)]
+}
+
 func main() {
-	createArgumentParser()
+	getRefs(createCommandFlags())
 }
 
